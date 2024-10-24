99e76377245e14f1b50aaaee24784ff33a69d6bc
heiress1 <aires.zheng1@gmail.com>
2024-10-24 18:28:25 -0400

    changed meme properties into a usestate object and created placed memedata in a state variable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
+
 export default function Header (){
+    
     return (
         <header>
             <h1 className="title">Meme Generator</h1>
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,42 +1,61 @@
-import React from "react"
+import memesData from '../memesData.jsx';
+import React, {useState} from "react";
+import ReactDom from "react-dom/client"
+import "../App.css";
+
 
 export default function Meme(){
+
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+
+    function getImage(){
+        // let x = Math.random();
+        const memesArray = allMemeImages.data.memes
+        let randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        return setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                randomImage: url}
+        })
+     }
+
     return(
-        <div>
-            <form className="form">
+        
+        <main>
+
+        
+            <div className="form">
                 {/* 34 28 */}
                     <div>
                         <label for="top"><b>Top Text</b></label><br></br>
-                    <input type="text" id="top" />
+                    <input className="text-base" type="text" id="top" />
                     </div>
-                    
-                
-
+                  
                     <div>
-                        <label for="bottom"><b>Bottom Text</b></label><br></br>
-                    <input type="text" id="bottom" />
+                        <label  for="bottom"><b>Bottom Text</b></label><br></br>
+                    <input className="text-base" type="text" id="bottom" />
                     </div>
-                    
+                        
+                     <button className="button " onClick={getImage}>Get a new meme image 🖼</button>
                 
+                     
                 
-                
-                     <button>Get a new meme image 🖼</button>
-                
-               
+        
+            </div>
+            <div className="imageContainer">
 
-               
-
-            </form>
-
-            {/* <button>Get a new meme image 🖼</button> */}
-            {/* <form className="submit">
-                <input type="submit" value="Get a new meme image"/>
-            </form > */}
-
-            {/* <form>
-                <label for="bottom">Bottom Text</label><br></br>
-                <input type="text" id="bottom" />
-            </form> */}
-        </div>
+                <img className="memeImage" src={meme.randomImage}/>
+            </div>
+            
+            
+        </main>   
     )
 }
