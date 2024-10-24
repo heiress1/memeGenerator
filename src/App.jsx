853059f737f5067fee9ee3aaa22ae1351a768b75
heiress1 <aires.zheng1@gmail.com>
2024-10-24 18:25:51 -0400

    updated css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
 import Meme from './components/Meme'
+import './index.css';
 
 function App() {
   
